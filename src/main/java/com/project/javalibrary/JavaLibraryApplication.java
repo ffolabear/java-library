f72c3d025f37fa3e15c8c03410de0d250ed3ae55
ffolabear <ffolabear@gmail.com>
2022-12-22 21:30:21 +0900

    feat: initial commit

---
+++ src/main/java/com/project/javalibrary/JavaLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.project.javalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLibraryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaLibraryApplication.class, args);
+	}
+
+}
