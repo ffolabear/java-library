26b6a2c1489edc6c4ad5bbdf8cbbfd3fe3243250
ffolabear <ffolabear@gmail.com>
2022-12-23 15:48:06 +0900

    feat(BookRepository): 도서관의 책을 저장하고 가져오는 클래스 추가

---
+++ src/main/java/com/project/javalibrary/domain/BookRepository.java
@@ -0,0 +1,33 @@
+package com.project.javalibrary.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class BookRepository {
+
+    private static final Map<Long, Book> bookStorage = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Book registerBook(Book book) {
+        book.setId(++sequence);
+        bookStorage.put(book.getId(), book);
+        return book;
+    }
+
+    public Book findById(Long id) {
+        return bookStorage.get(id);
+    }
+
+    public List<Book> finaAll() {
+        return new ArrayList<>(bookStorage.values());
+    }
+
+    public void clearStore() {
+        bookStorage.clear();
+    }
+}
